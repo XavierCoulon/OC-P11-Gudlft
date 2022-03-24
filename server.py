0f83ae0c83dccd2887f47d514c86f13258c0edb8
XavierCoulon <xcoulon.mail@gmail.com>
2022-03-24 15:08:00 +0100

    Fix BUG/Clubs_should_not_be_able_to_use_more_points

--- server.py
+++ server.py
@@ -37,24 +37,33 @@ def showSummary():
 
 
 @app.route('/book/<competition>/<club>')
-def book(competition,club):
+def book(competition, club):
     foundClub = [c for c in clubs if c['name'] == club][0]
     foundCompetition = [c for c in competitions if c['name'] == competition][0]
     if foundClub and foundCompetition:
-        return render_template('booking.html',club=foundClub,competition=foundCompetition)
+        return render_template('booking.html', club=foundClub, competition=foundCompetition)
     else:
         flash("Something went wrong-please try again")
         return render_template('welcome.html', club=club, competitions=competitions)
 
 
-@app.route('/purchasePlaces',methods=['POST'])
+@app.route('/purchasePlaces', methods=['POST'])
 def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
+    points = int(club["points"])
     placesRequired = int(request.form['places'])
-    competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
-    flash('Great-booking complete!')
+
+    if placesRequired <= points:
+        competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
+        points -= placesRequired
+        club["points"] = str(points)
+        flash('Great-booking complete!')
+    else:
+        flash('Not enough points!')
+
     return render_template('welcome.html', club=club, competitions=competitions)
+
 
 
 # TODO: Add route for points display
