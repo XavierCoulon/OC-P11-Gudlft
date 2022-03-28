d2cc49e54f1af18fceb4815da146e7d1ac2bdbe1
XavierCoulon <xcoulon.mail@gmail.com>
2022-03-28 14:10:00 +0200

    refactoring test + add test coverage

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,44 +1,8 @@
 import pytest
 import server
 
-from datetime import datetime
-
 
 @pytest.fixture()
 def client():
 	server.app.config["TESTING"] = True
 	return server.app.test_client()
-
-
-@pytest.fixture()
-def clubs():
-	clubs = [
-				{"name": "Club 1", "email": "club1@gmail.com", "points": "13"},
-				{"name": "Club 2", "email": "club2@gmail.com", "points": "12"},
-				{"name": "Club 3", "email": "club3@gmail.com", "points": "2"},
-				{"name": "Club 4", "email": "club4@gmail.com", "points": "0"},
-				{"name": "Club 5", "email": "club5@gmail.com", "points": "5"}
-			]
-	return clubs
-
-
-@pytest.fixture()
-def competitions():
-	competitions = [
-				{"name": "Competition 1", "date": "2023-10-22 13:30:00", "numberOfPlaces": "13"},
-				{"name": "Competition 2", "date": "2023-10-22 13:30:00", "numberOfPlaces": "4"},
-				{"name": "Competition 3", "date": "2023-10-22 13:30:00", "numberOfPlaces": "0"},
-				{"name": "Competition 5", "date": "2023-10-22 13:30:00", "numberOfPlaces": "20"},
-				{"name": "Competition 6", "date": "2020-10-22 13:30:00", "numberOfPlaces": "20"}
-			]
-	return competitions
-
-
-@pytest.fixture()
-def clubs_booking():
-	clubs_booking = {
-		"Club 1": {"Competition 1": 0, "Competition 2": 0, "Competition 3": 0},
-		"Club 2": {"Competition 1": 11, "Competition 2": 0, "Competition 3": 0},
-		"Club 3": {"Competition 1": 0, "Competition 2": 0, "Competition 3": 0}
-	}
-	return clubs_booking
